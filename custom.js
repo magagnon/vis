e18713ff179c2922808a87f5206fc64f8f1e79bf
Marc-Andre Gagnon <magagnon@carretechnologies.com>
2015-10-22 13:47:56 -0400

    Fixed var issue with _repaintEditButton, rebuilt the hx version to dist/vis-hexoskin

--- custom.js
+++ custom.js
@@ -4,7 +4,7 @@
 * We only need support for datasets and timelines
 *
 * Built using browserify:
-* browserify custom.js -o vis-custom.js -s vis -x moment
+* browserify custom.js -t babelify -o vis-hexoskin.js -s vis -x moment
 */
 
 // data
